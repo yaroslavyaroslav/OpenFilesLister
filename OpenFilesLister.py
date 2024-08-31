27c3ed95f76f1978f8dc606283a53bdf27bd99d7
Yaroslav Yashin <yaroslav.yashin@gmail.com>
2024-08-31 12:53:02 +0200

    Quick panel made persistent Code formatted

--- OpenFilesLister.py
+++ OpenFilesLister.py
@@ -2,6 +2,7 @@ import sublime_plugin
 import sublime
 import os
 
+
 class OpenFilesListerCommand(sublime_plugin.WindowCommand):
     def run(self):
         self.window = sublime.active_window()
@@ -11,7 +12,7 @@ class OpenFilesListerCommand(sublime_plugin.WindowCommand):
         self.fileNames = []
         self.viewIds = []
 
-        self.untitled = self.get_setting("show_untitled_files")
+        self.untitled = self.get_setting('show_untitled_files')
 
         count = 0
         active_group = self.window.active_group()
@@ -25,47 +26,59 @@ class OpenFilesListerCommand(sublime_plugin.WindowCommand):
                     self.fileNames.append(name.rsplit(os.sep, -1)[-1])
                     self.filePaths.append(name)
                     self.viewIds.append(sheet.view().id())
-                elif name == None and self.untitled == True:
+                elif name == None and self.untitled:
                     temp = sheet.view().name()
                     self.sheets.append(sheet.view())
-                    self.fileNames.append("Untitled-{}* {}".format(count, temp))
-                    self.filePaths.append("Untitled-{}{}".format(count, temp))
+                    self.fileNames.append('Untitled-{}* {}'.format(count, temp))
+                    self.filePaths.append('Untitled-{}{}'.format(count, temp))
                     self.viewIds.append(sheet.view().id())
                     count = count + 1
 
         id = self.window.active_sheet().view().id()
 
         if id not in self.viewIds:
-            self.window.run_command("next_view_in_stack")
+            self.window.run_command('next_view_in_stack')
             id = self.window.active_sheet().view().id()
 
         self.current = self.viewIds.index(id)
-        self.window.run_command("hide_overlay")
+        self.window.run_command('hide_overlay')
 
         self.show_panel()
 
     def show_panel(self):
-        self.window.show_quick_panel(self.fileNames, self.on_done, flags=sublime.WANT_EVENT, selected_index=self.current, on_highlight=self.on_highlighted)
+        self.window.show_quick_panel(
+            self.fileNames,
+            self.on_done,
+            flags=sublime.WANT_EVENT | sublime.KEEP_OPEN_ON_FOCUS_LOST,
+            selected_index=self.current,
+            on_highlight=self.on_highlighted,
+        )
 
     def set_timeout(self):
         sublime.set_timeout_async(lambda: self.close_panel(), 1500)
 
     def close_panel(self):
-        self.window.run_command("hide_overlay")
+        self.window.run_command('hide_overlay')
 
     def on_done(self, index, event):
         if index == -1:
             index = self.current
         if event.get('modifier_keys', None).get('primary', False):
-            self.window.open_file(fname=self.sheets[self.current].file_name(), flags=sublime.REPLACE_MRU)
-            self.window.open_file(fname=self.sheets[index].file_name(), flags=sublime.ADD_TO_SELECTION | sublime.CLEAR_TO_RIGHT)
+            self.window.open_file(
+                fname=self.sheets[self.current].file_name(), flags=sublime.REPLACE_MRU
+            )
+            self.window.open_file(
+                fname=self.sheets[index].file_name(),
+                flags=sublime.ADD_TO_SELECTION | sublime.CLEAR_TO_RIGHT,
+            )
         else:
             self.window.open_file(fname=self.sheets[index].file_name())
 
-
     def on_highlighted(self, index):
-        self.window.open_file(fname=self.sheets[index].file_name(), flags=sublime.SEMI_TRANSIENT | sublime.REPLACE_MRU)
-
+        self.window.open_file(
+            fname=self.sheets[index].file_name(),
+            flags=sublime.SEMI_TRANSIENT | sublime.REPLACE_MRU,
+        )
 
     def get_settings(self):
         return sublime.load_settings('OpenFilesLister.sublime-settings')
